e8e9ed881aa6d10bb23ffb5d150f531fad73eeac
JavierMijangos <mijangosjavier@hotmail.com>
2015-10-27 17:40:12 -0600

    Duel update
    Y hay transiciones entre los niveles e instrucciones del modo de juego.

---
+++ ProyectoFinal/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Z)){
+			Application.LoadLevel(Application.loadedLevel+1);
+		}
+	}
+}
