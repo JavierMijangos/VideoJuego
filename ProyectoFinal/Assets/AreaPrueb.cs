a6546665f909ba35aeee2bfb428127e30d7223dd
JavierMijangos <mijangosjavier@hotmail.com>
2015-11-06 18:46:40 -0600

    Actualización
    Player actualizado

---
+++ ProyectoFinal/Assets/AreaPrueb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaPrueb : MonoBehaviour {
+	public Transform parent;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.CompareTag("Player")){
+			//Debug.Log("Jugador entro en area");
+			parent.Translate (Vector3.left * Time.deltaTime);
+		}
+	}
+}
